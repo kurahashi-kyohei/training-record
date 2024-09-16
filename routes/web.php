1146c2c5267d71109b751c842c89624f022f4d6b
kurahashi-kyohei <kurakyo18@gmail.com>
2024-09-16 16:16:46 +0900

    fix

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,6 @@ Route::get('/', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
-// Route::get('/create', function () {
-//     return view('create');
-// })->middleware(['auth', 'verified'])->name('create');
-
-// Route::resource('history', TrainingController::class)->middleware(['auth', 'verified']);
 
 Route::prefix('history')
     ->middleware(['auth', 'verified'])
@@ -22,8 +17,7 @@ Route::prefix('history')
     ->group(function(){
         Route::get('/', 'index')->name('index');
         Route::post('/show', 'show')->name('show');
-        // Route::post('/', store)->name('store');
-    });
+});
 
 Route::prefix('create')
     ->middleware(['auth', 'verified'])
@@ -32,9 +26,8 @@ Route::prefix('create')
     ->group(function(){
         Route::get('/', 'createIndex')->name('index');
         Route::post('/', 'store')->name('store');
-    });
+});
 
-// Route::get('/history', [TrainingController::class, "index"])->middleware(['auth', 'verified'])->name('history');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
