52f97c2a427822b4a0f1e9432991329c5a4caf71
kurahashi-kyohei <kurakyo18@gmail.com>
2024-09-21 14:26:49 +0900

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies; 
+use Illuminate\Http\Middleware\TrustProxies; // 追加
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class, 
-             TrustProxies::class, 
+             TrustProxies::class, // 追加
 
         ]);
-        $middleware->trustProxies(at: '*'); 
+        $middleware->trustProxies(at: '*'); // 追加
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
