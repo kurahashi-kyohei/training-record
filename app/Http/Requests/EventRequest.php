acfb9eb34af6d1aa9538a9ae0fb1d8540535fcad
kurahashi-kyohei <kurakyo18@gmail.com>
2024-11-06 11:01:26 +0900

    fix

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -22,7 +22,7 @@ class EventRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required'],
+            'name' => ['required', 'unique:events'],
         ];
     }
 }
